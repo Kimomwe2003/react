4b0393b7b5a32d0e8f745aad839c5da00b02c42c
Kimomwe2003 <lidyakimomwe@gmail.com>
2025-01-29 13:32:55 +0300

    setting the endpoint

--- newapp/src/compnents/Header.js
+++ newapp/src/compnents/Header.js
@@ -4,20 +4,15 @@ import { Link } from "react-router-dom";
 function Header() {
     return (
       <div>
-        {/* Navbar */}
 <nav className="main-header navbar navbar-expand navbar-white navbar-light">
-  {/* Left navbar links */}
   <ul className="navbar-nav">
     <li className="nav-item">
-      {/* <a className="nav-link" data-widget="pushmenu" href="#" role="button"><i className="fas fa-bars" /></a> */}
     </li>
     <li className="nav-item d-none d-sm-inline-block">
       <Link to="/home" className="nav-link">Home</Link>
     </li>
     
   </ul>
-  {/* Right navbar links */}
-    {/* Messages Dropdown Menu */}
 
 </nav>
       </div>
